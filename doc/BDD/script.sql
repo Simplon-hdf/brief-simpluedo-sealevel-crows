6e13d800cd12465284d51f1e054120801e3a1bef
Messa <messaoud.houri@outlook.fr>
2024-11-22 15:40:58 +0100

    chore(doc/BDD): add stored procedure in script.sql

--- doc/BDD/script.sql
+++ doc/BDD/script.sql
@@ -97,6 +97,43 @@ AFTER INSERT OR UPDATE ON visiter
 FOR EACH ROW
 EXECUTE FUNCTION maj_position_trigger();
 
+/*Création de la procédure stockée "Lister tous les objets situés dans une pièce passée en paramètre" */
+
+CREATE OR REPLACE FUNCTION lister_objet(nom_salle VARCHAR)
+ RETURNS TABLE(nom_objets VARCHAR)
+ LANGUAGE sql
+ AS $$
+    SELECT nom_objets
+    FROM objets
+    INNER JOIN salles ON salles.id_salles = objets.id_salles
+    WHERE nom_salles = nom_salle;
+ $$;
+
+/* Création de la procédure stockée "Ajout d'un objet passé en paramètre et association avec la pièce concernée en ID" */
+CREATE OR REPLACE PROCEDURE ajout_objet_id_salle(nom_objets VARCHAR, id_salles INTEGER)
+ LANGUAGE plpgsql
+ AS $$
+ BEGIN
+     INSERT INTO objets (nom_objets, id_salles) VALUES (nom_objets, id_salles);
+ END;
+ $$;
+
+/* Création de la procédure stockée "Ajout d'un objet passé en paramètre et association avec la pièce concernée avec le nom" */
+ CREATE OR REPLACE PROCEDURE ajout_objet(var_nom_objets VARCHAR, var_nom_salles VARCHAR)
+ LANGUAGE plpgsql
+ AS $$
+ BEGIN
+     INSERT INTO objets (nom_objets, id_salles)
+     SELECT var_nom_objets, salles.id_salles
+     FROM salles
+     WHERE salles.nom_salles = var_nom_salles;
+ 
+     IF NOT FOUND THEN
+         RAISE EXCEPTION 'La salle "%" n''existe pas.', var_nom_salles;
+     END IF;
+ END;
+ $$;
+
 
 /* Ajout d'une colonne pour associer les utilisateurs aux rôles */
 ALTER TABLE utilisateurs ADD COLUMN id_roles INTEGER,
