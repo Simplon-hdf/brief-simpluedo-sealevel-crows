b6d6ad28a20b5305d49972cf32bfb9a3cdfec04b
UnknOownU <149084644+UnknOownU@users.noreply.github.com.>
2024-11-24 18:03:34 +0100

    docs(simpluedo-data.sql) add script for table visiter

--- doc/BDD/simpluedo-data.sql
+++ doc/BDD/simpluedo-data.sql
@@ -1,25 +1,41 @@
--- Pour se connecter à la DB avec l'utilisateur 
--- pgcli -U simpluedo_admin -p 5433 -d simpluedo_db
-
-/* Connexion à la BDD */
+-- Connexion à la BDD
 \c simpluedo_db
 
-/* Insertion de données dans la table rôles */
+-- Insertion de données dans la table "roles"
 INSERT INTO roles (nom_roles) VALUES
  ('observateur'),
  ('utilisateur'),
  ('maitre du jeu');
 
-/* Insertion de données dans la table personnages */
+-- Insertion de données dans la table "personnages"
 INSERT INTO personnages (nom_personnages) VALUES
 ('Colonel Moutarde'),
-('Docteur OLIVE'),
-('Professeur VIOLET'),
-('Madame PERVENCHE'),
-('Mademoiselle ROSE'),
-('Madame LEBLANC');
+('Docteur Olive'),
+('Professeur Violet'),
+('Madame Pervenche'),
+('Mademoiselle Rose'),
+('Madame Leblanc');
 
-/* Insertion de données dans la table salles */
+-- Insertion de données dans la table "utilisateurs" avec personnages associés
+INSERT INTO utilisateurs (pseudo_utilisateurs, id_roles, id_personnages) VALUES
+('MessaKami', 3, 1),
+('GETAMAZIGHT', 2, 3),
+('Srekaens', 2, 2),
+('Kuro', 2, 5),
+('Shotax', 2, 6),
+('Jegoro', 2, 4);
+
+-- Insertion de données dans la table "utilisateurs" sans personnages
+INSERT INTO utilisateurs (pseudo_utilisateurs, id_roles) VALUES
+('Martial', 1),
+('Aurore', 1),
+('Julien', 1),
+('Boris', 1),
+('Gabriel', 1),
+('Yohan', 1),
+('Franck', 1);
+
+-- Insertion de données dans la table "salles"
 INSERT INTO salles (nom_salles) VALUES
 ('Cuisine'),
 ('Grand Salon'),
@@ -31,7 +47,7 @@ INSERT INTO salles (nom_salles) VALUES
 ('Véranda'),
 ('Salle à manger');
 
-/* Insertion de données dans la table objets */
+-- Insertion de données dans la table "objets"
 INSERT INTO objets (nom_objets, id_salles) VALUES
 ('Poignard', 4),
 ('Revolver', 6),
@@ -40,21 +56,11 @@ INSERT INTO objets (nom_objets, id_salles) VALUES
 ('Clé anglaise', 8),
 ('Matraque', 8);
 
-/* Insertion de données dans la table utilisateurs */
-INSERT INTO utilisateurs (pseudo_utilisateurs, id_roles, id_personnages) VALUES
-('MessaKami', 3, 1),
-('GETAMAZIGHT', 2, 3),
-('Srekaens', 2, 2),
-('Kuro', 2, 5),
-('Shotax', 2, 6),
-('Jegoro', 2, 4);
-
-/* Insertion de données dans la table utilisateurs */
-INSERT INTO utilisateurs (pseudo_utilisateurs, id_roles) VALUES
-('Martial', 1),
-('Aurore', 1),
-('Julien', 1),
-('Boris', 1),
-('Gabriel', 1),
-('Yohan', 1),
-('Franck', 1);
+-- Insertion de données dans la table "visiter"
+INSERT INTO visiter (id_personnages, id_salles, heure_arrivee, heure_sortie) VALUES
+(1, 1, '08:00', '08:30'),  
+(2, 4, '08:15', '08:45'),
+(3, 5, '08:30', '09:00'), 
+(4, 1, '08:20', '08:50'),
+(5, 3, '09:00', '09:30'), 
+(6, 6, '09:15', '09:45');
